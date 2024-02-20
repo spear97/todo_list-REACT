74c1686213f7817bc82c0647047422593cdbeeec
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 13:45:49 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'; // Import CSS file for styling
+import App from './App'; // Import the main App component
+import reportWebVitals from './reportWebVitals'; // Import the function to report web vitals
 
+// Create a root element for React rendering
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside React StrictMode for additional checks and warnings in development mode
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measure performance in the application by passing a function to log results
+// For example, you can log results to the console or send them to an analytics endpoint
+// Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
